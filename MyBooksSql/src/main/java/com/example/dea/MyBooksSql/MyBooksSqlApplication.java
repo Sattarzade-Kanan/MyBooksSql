cdb9a804518cb3b7170c5ea88028e83cf856c520
Sattarzade-Kanan <chinkan1980@gmail.com>
2025-11-26 14:35:36 +0400

    Reapet SPringg web

---
+++ MyBooksSql/src/main/java/com/example/dea/MyBooksSql/MyBooksSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.dea.MyBooksSql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBooksSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBooksSqlApplication.class, args);
+	}
+
+}
---
+++ MyBooksSql/src/test/java/com/example/dea/MyBooksSql/MyBooksSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dea.MyBooksSql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBooksSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
